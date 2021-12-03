85470755b2c0f86c2b06d1ab64d058b742185137
afeexclusive <afeexclusive@gmail.com>
2021-12-03 11:39:10 +0100

    extractor source code

---
+++ ExtractNumberFromString/NumberExtractor/NumberExtractor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringContainingNumber = Guid.NewGuid().ToString();
+            string extractedNumber = ExtractNumberFromString(stringContainingNumber);
+
+            Console.WriteLine($"Original String = {stringContainingNumber}");
+            Console.WriteLine($"Extracted Number = {extractedNumber}");
+        }
+
+        private static string ExtractNumberFromString(string str)
+        {
+            return new string(str.Where(char.IsDigit).ToArray());
+        }
+
+    }
+}
